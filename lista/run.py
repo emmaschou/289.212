3f409a81503e84419a6f5d8b7be48e41b2fc5f07
emmaschou <schouemm@gmail.com>
2018-08-15 10:53:50 +1200

    added working run.py

--- lista/run.py
+++ lista/run.py
@@ -3,17 +3,39 @@ import sqlite3
 
 app = Flask(__name__)
 
-
 PLAYLISTDB = 'playlist.db'
 
-print(PLAYLISTDB)
+
+@app.route('/')
+def index():
+  # con = sqlite3.connect(PLAYLISTDB)
+  # playlist = createplaylist(con)
+  # con.close()
+
+  return render_template('index.html')
 
 
-@app.route('/createdlista')
-def createplaylist():
-    playlist = []
 
-    con = sqlite3.connect(PLAYLISTDB)
-    cur = con.execute('SELECT song, artist, album FROM songs WHERE mood=="on" AND idk)
-    for row in cur:
-      songs.append(list(row))
+@app.route('/createdlista', methods=['POST'])
+def createdlista():
+  query = 'where '
+
+  for name in request.form:
+    if name[0:3] == 'gen':
+      query += ('genre = ' + '\'' + request.form[name] + '\'' + ' and ')
+    if name[0:3] == 'col':
+      query += ('color = ' + '\'' + request.form[name] + '\'' + ' and ')
+    if name[0:3] == 'moo':
+      query += ('mood = ' + '\'' + request.form[name] + '\'' + ' and ')
+
+  query = query[0:-5]
+
+  playlist = []
+
+  con = sqlite3.connect(PLAYLISTDB)
+  cur = con.execute('select * from songs ' + query)
+  for row in cur:
+    playlist.append(list(row))
+  con.close()
+
+  return render_template('createdlista.html', playlist=playlist, query=query)
